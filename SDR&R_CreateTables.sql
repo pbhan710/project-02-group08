361dd9b962f34f8898e99948c7975ed117f14ac4
pbhan710 <pbhan710@gmail.com>
2022-12-08 20:57:05 -0500

    Renamed table variables.

--- SDR&R_CreateTables.sql
+++ SDR&R_CreateTables.sql
@@ -6,28 +6,28 @@
 -- To reset the sample schema, replace everything with
 -- two dots ('..' - without quotes).
 
-CREATE TABLE std (
-    indicator varchar NOT NULL,
-    geography varchar NOT NULL,
-    year int NOT NULL,
-    number_of_std_cases int NOT NULL,
-    std_rate_per_100000 float NOT NULL
+CREATE TABLE "sex" (
+    "indicator" varchar NOT NULL,
+    "geography" varchar NOT NULL,
+    "year" int NOT NULL,
+    "number_of_std_cases" int NOT NULL,
+    "std_rate_per_100000" float NOT NULL
 );
 
-CREATE TABLE dpm (
-    state varchar NOT NULL,
-    year int NOT NULL,
-    deaths int NOT NULL,
-    population int NOT NULL,
-    crude_death_rate float NOT NULL,
-    standard_error_for_crude_rate float NOT NULL,
-    lower_confidence_limit_for_crude_rate float NOT NULL,
-    upper_confidence_limit_for_crude_rate float NOT NULL,
-    age_adjusted_rate float NOT NULL,
-    standard_error_for_age_adjusted_rate float NOT NULL,
-    lower_confidence_limit_for_age_adjusted_rate float NOT NULL,
-    upper_confidence_limit_for_age_adjusted_rate float NOT NULL,
-    state_crude_rate_in_range varchar NOT NULL,
-    us_crude_rate float NOT NULL,
-    us_age_adjusted_rate float NOT NULL
+CREATE TABLE "drugs" (
+    "state" varchar NOT NULL,
+    "year" int NOT NULL,
+    "deaths" int NOT NULL,
+    "population" int NOT NULL,
+    "crude_death_rate" float NOT NULL,
+    "standard_error_for_crude_rate" float NOT NULL,
+    "lower_confidence_limit_for_crude_rate" float NOT NULL,
+    "upper_confidence_limit_for_crude_rate" float NOT NULL,
+    "age_adjusted_rate" float NOT NULL,
+    "standard_error_for_age_adjusted_rate" float NOT NULL,
+    "lower_confidence_limit_for_age_adjusted_rate" float NOT NULL,
+    "upper_confidence_limit_for_age_adjusted_rate" float NOT NULL,
+    "state_crude_rate_in_range" varchar NOT NULL,
+    "us_crude_rate" float NOT NULL,
+    "us_age_adjusted_rate" float   NOT NULL
 );
--- SDR&R_JoinTables.sql
+++ SDR&R_JoinTables.sql
@@ -9,36 +9,36 @@ select
 	chlamydia_cases,
 	congenital_syphilis_cases,
 	early_non_primary_secondary_syphilis_cases,
-	gonorrhea_cases
+	gonorrhea_cases,
 	primary_secondary_syphilis_cases
-from dpm as d
+from drugs as d
 left join (
 	select indicator, geography, year, number_of_std_cases as "chlamydia_cases"
-	from std
+	from sex
 	where indicator = 'Chlamydia'
 ) as ch
 	on (d.state = ch.geography) and (d.year = ch.year)
 left join (
 	select indicator, geography, year, number_of_std_cases as "congenital_syphilis_cases"
-	from std
+	from sex
 	where indicator = 'Congenital Syphilis'
 ) as cs
 	on (d.state = cs.geography) and (d.year = cs.year)
 left join (
 	select indicator, geography, year, number_of_std_cases as "early_non_primary_secondary_syphilis_cases"
-	from std
+	from sex
 	where indicator = 'Early Non-Primary, Non-Secondary Syphilis'
 ) as ens
 	on (d.state = ens.geography) and (d.year = ens.year)
 left join (
 	select indicator, geography, year, number_of_std_cases as "gonorrhea_cases"
-	from std
+	from sex
 	where indicator = 'Gonorrhea'
 ) as g
 	on (d.state = g.geography) and (d.year = g.year)
 left join (
 	select indicator, geography, year, number_of_std_cases as "primary_secondary_syphilis_cases"
-	from std
+	from sex
 	where indicator = 'Primary and Secondary Syphilis'
 ) as s
 	on (d.state = s.geography) and (d.year = s.year)
